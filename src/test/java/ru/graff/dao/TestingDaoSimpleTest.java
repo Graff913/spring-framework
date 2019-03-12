a82fecc4e9118453e4393f8ca27d80f6e77352c0
graff913 <graff913@outlook.com>
2019-03-12 23:28:38 +0300

    Исправления по второму заданию

--- src/test/java/ru/graff/dao/TestingDaoSimpleTest.java
+++ src/test/java/ru/graff/dao/TestingDaoSimpleTest.java
@@ -1,38 +1,40 @@
 package ru.graff.dao;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.test.context.ContextConfiguration;
-import ru.graff.conf.MessageConf;
 
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-@ContextConfiguration(classes = MessageConf.class)
 class TestingDaoSimpleTest {
-
-    private TestingDao testingDao = new TestingDaoSimple();
 
     @Test
     void readQuestionsNotFoundFile() {
+        TestingDao testingDao = new TestingDaoSimple("tests_not_found.csv");
         Locale locale = new Locale("ru", "RU");
-        //assertThrows(NullPointerException.class, () -> testingDao.readQuestions(locale));
+        assertThrows(NullPointerException.class, () -> testingDao.readQuestions(locale));
     }
 
     @Test
     void getQuestion() {
+        TestingDao testingDao = new TestingDaoSimple("tests.csv");
         testingDao.readQuestions(Locale.ENGLISH);
-        assertEquals("Q1", testingDao.getQuestion(0));
+        assertEquals("Q1\nA1, A2, A3, A4", testingDao.getQuestion(0).toString());
+        assertEquals("Q2\nA1, A2, A3, A4", testingDao.getQuestion(1).toString());
+        assertEquals("Q3\nA1, A2, A3, A4", testingDao.getQuestion(2).toString());
+        assertEquals("Q4\nA1, A2, A3, A4", testingDao.getQuestion(3).toString());
+        assertEquals("Q5\nA1, A2, A3, A4", testingDao.getQuestion(4).toString());
     }
 
     @Test
     void checkAnswer() {
+        TestingDao testingDao = new TestingDaoSimple("tests.csv");
+        testingDao.readQuestions(Locale.ENGLISH);
+        assertTrue(testingDao.checkAnswer(0, 1));
+        assertFalse(testingDao.checkAnswer(1, 4));
+        assertTrue(testingDao.checkAnswer(2, 3));
+        assertTrue(testingDao.checkAnswer(3, 4));
+        assertFalse(testingDao.checkAnswer(4, 5));
     }
+
 }
